14ffa1f0e32af640a3b1e022282e08d6023c36e1
fLUM1NUS <dearva@yandex.ru>
2022-12-09 12:11:24 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption("Виртуальное казино")  # Название приложения
+    size = width, height = 1600, 900  # размеры окна
+    screen = pygame.display.set_mode(size)
+    programIcon = pygame.image.load('res/icon.png')  # Иконка приложения
+    pygame.display.set_icon(programIcon)
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
